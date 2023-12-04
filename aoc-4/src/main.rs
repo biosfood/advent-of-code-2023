75bcb1d4529b51dac8014bede3619997505d5968
biosfood <leisenhau@gmail.com>
2023-12-04 10:29:56 +0100

    add aoc-4 part 2 solution

--- aoc-4/src/main.rs
+++ aoc-4/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs;
 use std::iter;
 use regex::Regex;
+use std::cmp::min;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,7 +17,8 @@ fn main() {
 
     let card: Regex = Regex::new(r"Card\s*(\d+): ([\d| ]*)\|(.*)").unwrap();
     let number: Regex = Regex::new(r"\d+").unwrap();
-    let mut sum = 0;
+    let mut sum1 = 0;
+    let mut sum2 = 0;
     let number_of_original_cards = lines.len();
     let mut number_of_cards: Vec<usize> = iter::repeat(1).take(number_of_original_cards).collect();
     for line in lines {
@@ -26,6 +28,7 @@ fn main() {
         let winning_numbers = number.captures_iter(&card_result[2]).map(|x| x[0].parse::<u32>().unwrap()).collect::<Vec<u32>>();
         let values = number.captures_iter(&card_result[3]).map(|x| x[0].parse::<u32>().unwrap()).collect::<Vec<u32>>();
         let mut number_of_matches = 0;
+        let card_number = &card_result[1].parse::<usize>().unwrap();
         for value in &values {
             if winning_numbers.contains(&value) {
                 number_of_matches += 1;
@@ -33,7 +36,14 @@ fn main() {
         }
         let points = if number_of_matches == 0 { 0 } else { 1 << (number_of_matches - 1) };
         println!("card {}: winning: {}, values: {}, {} matches => {}", &card_result[1], winning_numbers.len(), values.len(), number_of_matches, points);
-        sum += points;
+        sum1 += points;
+        for i in *card_number..min(card_number+number_of_matches, number_of_original_cards) {
+            number_of_cards[i] += number_of_cards[card_number - 1];
+        }
     }
-    println!("sum of points: {}", sum);
+    for cards in number_of_cards {
+        println!("{cards}");
+        sum2 += cards;
+    }
+    println!("sum of points: {}, number of cards: {}", sum1, sum2);
 }
