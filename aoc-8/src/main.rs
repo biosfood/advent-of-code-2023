93babf53ce3f933f470ebb8e06a24b4b3602a619
biosfood <leisenhau@gmail.com>
2023-12-08 09:22:58 +0100

    aoc-8 part 2: probably functional version but takes far too long to run

--- aoc-8/src/main.rs
+++ aoc-8/src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 use regex::Regex;
 use std::collections::HashMap;
 
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     assert_eq!(args.len(), 2, "Expected exactly one argument");
@@ -28,17 +29,38 @@ fn main() {
         nodes.insert(key, (left, right));
     }
 
-    let mut i = 0;
+    let mut i1 = 0;
     let mut current_node = "AAA";
     while current_node != "ZZZ" {
-        let direction = instructions.chars().nth(i % instructions.len()).unwrap();
+        let direction = instructions.chars().nth(i1 % instructions.len()).unwrap();
         let (left, right) = nodes.get(current_node).unwrap();
         if direction == 'R' {
             current_node = right;
         } else {
             current_node = left;
         }
-        i += 1;
+        i1 += 1;
     }
-    println!("Took {} steps for part one!", i);
+
+    let mut i2 = 0;
+    let mut current_nodes = nodes.keys().filter(|node| node.chars().nth(node.len()-1).unwrap() == 'A').map(|node| node).collect::<Vec<&String>>();
+    println!("starting at {} nodes that end with an A", current_nodes.len());
+    let mut should_continue = true;
+    while should_continue {
+        let direction = instructions.chars().nth(i2 % instructions.len()).unwrap();
+        let new_nodes = current_nodes.iter().map(|node| 
+            if direction == 'R' { &nodes.get(*node).unwrap().1 }
+            else { &nodes.get(*node).unwrap().0 }).collect::<Vec<&String>>();
+        current_nodes = new_nodes;
+
+        should_continue = false;
+        for node in &current_nodes {
+            if node.chars().nth(node.len()-1).unwrap() != 'Z' {
+                should_continue = true;
+                break;
+            }
+        }
+        i2 += 1;
+    }
+    println!("Took {} steps for part one, {} for part two", i1, i2);
 }
