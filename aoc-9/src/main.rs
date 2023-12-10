72032f61f1e162811892d25e6082e061504afe29
biosfood <leisenhau@gmail.com>
2023-12-10 12:00:45 +0100

    add aoc-9 part 1

---
+++ aoc-9/src/main.rs
@@ -0,0 +1,46 @@
+use std::env;
+use std::fs;
+use regex::Regex;
+
+fn compute_derivatives(history: Vec<isize>) -> Vec<Vec<isize>> {
+    let mut result: Vec<Vec<isize>> = Vec::new();
+    result.push(history);
+
+    while result[result.len()-1].iter().any(|x| *x != 0) {
+        let previous = &result[result.len()-1];
+        let mut next = Vec::<isize>::new();
+        for i in 0..(previous.len()-1) {
+            next.push(previous[i+1] - previous[i]);
+        }
+        result.push(next);
+    }
+    result
+}
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    assert_eq!(args.len(), 2, "Expected exactly one argument");
+    let file_path = &args[1];
+    println!("Reading file {file_path}");
+
+    let lines: Vec<String> = fs::read_to_string(file_path).unwrap_or_else(|_| {
+        panic!("Could not read file {file_path}");
+    }).lines().map(String::from).collect();
+    let number_regex = Regex::new(r"-?\d+").unwrap();
+    let histories = lines.iter().filter(|line| !line.is_empty()). map(|line| {
+        let result = number_regex.captures_iter(line).map(|result| result[0].parse::<isize>().unwrap()).collect::<Vec<isize>>();
+        result
+    });
+    let derivatives = histories.map(compute_derivatives).collect::<Vec<Vec<Vec<isize>>>>();
+    let mut sum = 0;
+    for value in derivatives {
+        let mut next_derivative = 0;
+        for i in (0..value.len()).rev() {
+            next_derivative = value[i][value[i].len() - 1] + next_derivative;
+        }
+        sum += next_derivative;
+        println!("next value: {}", next_derivative);
+    }
+    println!("sum: {}", sum);
+}
