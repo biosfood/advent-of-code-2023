ce86b75a8209c08e9daaa17e207b28b7c7e1b20c
biosfood <leisenhau@gmail.com>
2023-12-12 23:39:11 +0100

    add aoc-12 part 2 solution

--- aoc-12/src/main.rs
+++ aoc-12/src/main.rs
@@ -1,8 +1,9 @@
 use std::env;
 use std::fs;
+use std::collections::HashMap;
 use regex::Regex;
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 enum State {
     Operational,
     Damaged,
@@ -20,6 +21,50 @@ impl State {
     }
 }
 
+fn get_solution_count(states: &[State], groups: &[usize], preceding_damaged: usize, cache: &mut HashMap<(Vec<State>, Vec<usize>, usize), usize>) -> usize {
+    if states.len() == 0{
+        return if groups.len() == 0 && preceding_damaged == 0 || groups.len() == 1 && groups[0] == preceding_damaged {
+            1
+        } else {
+            0
+        }
+    }
+    let key = (states.to_vec(), groups.to_vec(), preceding_damaged);
+    if cache.contains_key(&key) {
+        return *cache.get(&key).unwrap()
+    }
+    let result = match states[0] {
+        State::Damaged => {
+            get_solution_count(&states[1..], &groups[..], preceding_damaged + 1, cache)
+        }
+        State::Operational => {
+            if preceding_damaged != 0 {
+                if groups.len() == 0 || groups[0] != preceding_damaged {
+                    0
+                } else {
+                    get_solution_count(&states[1..], &groups[1..], 0, cache)
+                }
+            } else {
+                get_solution_count(&states[1..], &groups[..], 0, cache)
+            }
+        }
+        State::Unknown => {
+            get_solution_count(&states[1..], &groups[..], preceding_damaged + 1, cache) +
+            if preceding_damaged != 0 {
+                if groups.len() == 0 || groups[0] != preceding_damaged {
+                    0
+                } else {
+                    get_solution_count(&states[1..], &groups[1..], 0, cache)
+                }
+            } else {
+                get_solution_count(&states[1..], &groups[..], 0, cache)
+            }
+        }
+    };
+    cache.insert(key, result);
+    result
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     assert_eq!(args.len(), 2, "Expected exactly one argument");
@@ -31,47 +76,28 @@ fn main() {
     }).lines().map(String::from).filter(|line| !line.is_empty()).collect();
 
     let parts_regex = Regex::new(r"(?<records>[\.#\?]+) (?<groups>(\d+,)*\d+)").unwrap();
-    let mut soulution_count = 0;
+    let mut solution_count = 0;
+    let mut unfolded_count = 0;
     for line in lines {
         let result = parts_regex.captures(&line).unwrap();
-        let groups = result["groups"].split(",").map(str::parse).map(Result::unwrap).collect::<Vec<isize>>();
+        let groups = result["groups"].split(",").map(str::parse).map(Result::unwrap).collect::<Vec<usize>>();
         let states = result["records"].chars().map(State::from_char).collect::<Vec<State>>();
-        let number_of_unknowns = states.iter().filter(|state| **state == State::Unknown).count();
-        // println!("{:?}: {:?}, {} unknowns", states, groups, number_of_unknowns);
-        for i in 0..(1 << (number_of_unknowns)) {
-            let mut unknown_index = 0;
-            let current_states = states.iter().map(|state| {
-                if *state == State::Unknown {
-                    if i & 1 << unknown_index == 0 {
-                        unknown_index += 1;
-                        State::Operational
-                    } else {
-                        unknown_index += 1;
-                        State::Damaged
-                    }
-                } else {
-                    *state
-                }
-            }).collect::<Vec<State>>();
-            let mut current_groups = Vec::<isize>::new();
-            let mut current_length = 0;
-            for state in current_states.iter() {
-                if *state == State::Damaged {
-                    current_length += 1;
-                } else if current_length != 0 {
-                    current_groups.push(current_length);
-                    current_length = 0;
-                }
-            }
-            if current_length > 0 {
-                current_groups.push(current_length);
-            }
-            // println!("{:?} -> {:?}", current_states, current_groups);
-            if current_groups == groups {
-                soulution_count += 1;
-                // println!("Found solution: {:?}", current_states);
-            }
+        let mut cache: HashMap<(Vec<State>, Vec<usize>, usize), usize> = HashMap::new();
+        let solutions = get_solution_count(states.as_slice(), groups.as_slice(), 0, &mut cache);
+        let mut unfolded_states: Vec<State> = Vec::new();
+        let mut unfolded_groups: Vec<usize> = Vec::new();
+        for _i in 0..4 {
+            unfolded_states.append(&mut states.clone());
+            unfolded_states.push(State::Unknown);
+            unfolded_groups.append(&mut groups.clone());
         }
+        unfolded_states.append(&mut states.clone());
+        unfolded_groups.append(&mut groups.clone());
+        let mut cache: HashMap<(Vec<State>, Vec<usize>, usize), usize> = HashMap::new();
+        let unfolded = get_solution_count(unfolded_states.as_slice(), unfolded_groups.as_slice(), 0, &mut cache);
+        println!("Found {solutions} solutions part 1, unfolded: {}", unfolded);
+        solution_count += solutions;
+        unfolded_count += unfolded;
     }
-    println!("Found {} solutions", soulution_count);
+    println!("Found {} solutions, unfolded: {}", solution_count, unfolded_count);
 }
