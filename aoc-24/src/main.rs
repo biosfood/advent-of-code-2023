3e605c60ab99f221ef13ac1ff879167b879830c8
biosfood <leisenhau@gmail.com>
2023-12-24 23:18:48 +0100

    add aoc-24 part two (solution?)

--- aoc-24/src/main.rs
+++ aoc-24/src/main.rs
@@ -49,7 +49,7 @@ impl Particle {
     }
 
     fn get_collision_time(&self, other: &Particle) -> (f128, f128) {
-        let t_1_divisor = (self.velocity[0] * other.velocity[1] - self.velocity[1] * other.velocity[0]);
+        let t_1_divisor = self.velocity[0] * other.velocity[1] - self.velocity[1] * other.velocity[0];
         if t_1_divisor == (0) {
             return (f128::from(-1), f128::from(-1));
         }
@@ -58,10 +58,8 @@ impl Particle {
         let b = f128::from(other.velocity[1]) / f128::from(other.velocity[0]);
         let d = f128::from(other.position[1]) - b * f128::from(other.position[0]);
         let p1 = (d-c)/(a-b);
-        let p2 = a*(d-c)/(a-b) + c;
         let t1 = (p1-f128::from( self.position[0])) / f128::from( self.velocity[0]);
         let t2 = (p1-f128::from(other.position[0])) / f128::from(other.velocity[0]);
-        println!("t1: {}, t2: {}, p1: {}, p2: {}", t1, t2, p1, p2);
         (t1, t2)
     }
 }
@@ -85,8 +83,6 @@ fn main() {
             if t1 < f128::from(0) || t2 < f128::from(0) {
                 continue;
             }
-            // println!("Particle {} and {} collide at {} and {}", i, j, t1, t2);
-            // let position = (0..2).map(|index| particles[i].position[index] as i128 + t1 * particles[i].velocity[index] as i128).collect::<Vec<i128>>();
             if t1 < particles[i].collision_time && t2 < particles[j].collision_time {
                 particles[i].collision_time = t1;
                 particles[i].collision_partner = j;
@@ -104,35 +100,13 @@ fn main() {
                 continue;
             }
             intersections += 1;
-            // let position1 = (0..2).map(|index| (particles[i].position[index]) + t1 * (particles[i].velocity[index]));
-            // let position2 = (0..2).map(|index| (particles[j].position[index]) + t2 * (particles[j].velocity[index]));
-            // println!("Particle {} and {} collide at {:?} and {:?}", i, j, position1.collect::<Vec<i128>>(), position2.collect::<Vec<i128>>());
         }
     }
     println!("Intersections: {}", intersections);
-    let mut collisions = 0;
-    for i in 0..particles.len() {
-        let particle = &particles[i];
-        if particle.collision_partner == usize::MAX || particle.collision_time < f128::from(0) {
-            // no collision (weird)
-            continue;
+    println!("Please use a smarter piece of software to solve this system of equations: ");
+    for i in 0..3 {
+        for j in 0..3 {
+            println!("p{j} + v{j} * t{i} == {} + {} * t{i} &&", particles[i].position[j], particles[i].velocity[j]);
         }
-        if particles[particle.collision_partner].collision_partner != i {
-            // println!("particle {} and {} are not colliding, because another collision occured earlier", i, particle.collision_partner);
-            continue;
-        }
-        let position = (0..2).map(|index| f128::from(particles[i].position[index]) + particle.collision_time * f128::from(particles[i].velocity[index])).collect::<Vec<f128>>();
-        let mut in_bounds = true;
-        for pos in &position {
-            if *pos < f128::from(200000000000000 as i128) || *pos > f128::from(400000000000000 as i128) {
-                in_bounds = false;
-            }
-        }
-        if !in_bounds {
-            continue;
-        }
-        println!("Particle {} and {} collide at {:?}", i, particle.collision_partner, position);
-        collisions += 1;
     }
-    println!("{} collisions (really {})", collisions, collisions / 2);
 }
